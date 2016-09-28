5078b30282da4935bd313c01008043d210b0dc38
Kevin Holland <hollandk@oclc.org>
2016-09-28 12:32:39 -0400

    Updating config.fish

--- config.fish
+++ config.fish
@@ -1,63 +1,24 @@
 #----------------------
-#fish start-up script |
+#fish start- script |
 #----------------------
+
+# Path to Oh My Fish install.
+set -gx OMF_PATH $HOME/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG $HOME/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
 
 #add typesafe activator to path
 set -gx PATH ~/activator $PATH
 
-#-----FUNCTIONS-----------------
-#"school alias" function
-#navigates to current semester
-function sa -d "Navigates to current semester directory"
-    cd ~kevinholland/Documents/School/Current-Year4/Current-Semester1/
-end
+#set default bd option to case insensitive (-i) implies seems mode (-s)
+set -gx BD_OPT 'insensitive'
 
-function cl -d "Change directory and list"
+function cl -d "Change to directory and do ls"
     cd $argv; ls
-end
-
-#mysql functions
-function mysql_start -d "Starts mysql server"
-    /Library/MySQL/bin/mysql.server start
-end
-function mysql_stop -d "Stops mysql server"
-    /Library/MySQL/bin/mysql.server stop
-end
-
-#tomcat functions
-function tomcat_start -d "Starts tomcat instance"
-    /Library/Tomcat/bin/startup.sh
-end
-function tomcat_stop -d "Stops tomcat instance"
-    /Library/Tomcat/bin/shutdown.sh
-end
-#get tomcat port
-function tomcat_port -d "Returns tomcat port instance from server.xml"
-    cat /Library/Tomcat/conf/server.xml\
-     | grep 'Connector' \
-     | grep 'protocol=\"HTTP/1.1\"'\
-     | awk '{print $2}'\
-     | sed 's/[^0-9]//g'
-end
-#check tomcat status (localhost:9080)
-function tomcat_status -d "Hits localhost:{port} and outputs response"
-    echo "tomcat status:"
-    tomcat_port | read port
-    curl -s -o /dev/null -w " ->%{url_effective}\n <-%{http_code}\n" localhost:"$port"
-end
-
-#web servers
-function webserv_start -d "Starts tomcat and mysql"
-    echo "Starting web servers"
-    /Library/Tomcat/bin/startup.sh
-    /Library/MySQL/bin/mysql.server start
-    echo "Success"
-end
-function webserv_stop -d "Stops tomcat and mysql"
-    echo "Stopping web servers"
-    /Library/Tomcat/bin/shutdown.sh
-    /Library/MySQL/bin/mysql.server stop
-    echo "Success"
 end
 
 #ip functions
@@ -86,4 +47,9 @@ function gacp -d "git add commit push"
         echo "Expected a commit message argument"
     end
 end
+
+echo "Here is a fortune for the hard times: "
+echo ""
+cowsay (fortune)
+echo ""
 
