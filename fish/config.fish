61bc1d65af0e9afbb6b3b693683d1dd93b5c23d5
Kevin Holland <hollandk@oclc.org>
2018-07-24 09:58:38 -0400

    saving for new laptop

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,8 @@
 #fish start- script |
 #----------------------
 
+set -gx PATH ~/bin $PATH
+
 # Path to Oh My Fish install.
 set -gx OMF_PATH $HOME/.local/share/omf
 
@@ -22,14 +24,29 @@ set -gx OMF_CONFIG $HOME/.config/omf
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
 
-#add typesafe activator to path
-set -gx PATH ~/activator $PATH
-
 #set default bd option to case insensitive (-i) implies seems mode (-s)
 set -gx BD_OPT 'insensitive'
 
 function halp -d "Alias for fuck"
     fuck
+end
+
+function fucku -d "Alias for fuck -y"
+    fuck -y
+end
+
+#rat alias
+function rat -d "Runs rat"
+    ~/go/bin/rat
+end
+#git/rat alias
+function gitv -d "Runs rat also"
+    rat
+end
+
+#mosh to asteria
+function aster
+	mosh bskcx@asteria.duckdns.org --ssh="ssh -p 56123"
 end
 
 #ip functions
@@ -84,8 +101,26 @@ function kbdon -d "Turn keyboard on"
     sudo kextload /System/Library/Extensions/AppleUSBTopCase.kext/Contents/PlugIns/AppleUSBTCKeyboard.kext/
 end
 
-function dank -d "danks up a string to ur clipboard"
-    groovy ~/dank.groovy $argv
+function dui -d "Takes you to DUI source"
+    echo 'WARPING TO DUI SOURCE'
+    cd /Volumes/SourceCode/source/discoveryui
+end
+function ds -d "Takes you to DS source"
+    echo 'WARPING TO DS SOURCE'
+    cd /Volumes/SourceCode/source/discoveryservices
+end
+function dr -d "Takes you to DR source"
+    echo 'WARPING TO DR SOURCE'
+    cd /Volumes/SourceCode/source/deliveryresolver
+end
+
+#curl aliases
+function cxml -d "Curls with Accept: application/xml"
+    curl -H "Accept: application/xml" $argv
+end
+
+function cjson -d "Curls with Accept: application/json"
+    curl -H "Accept: application/json" $argv
 end
 
 echo "           | keveloper |"
